f1902638f3c6b05dadb839e0a51f2d7124d00077
prorock201743 <zolotm24@gmail.com>
2021-11-11 21:03:53 +0300

    Добавьте файлы проекта.

---
+++ Practice_6/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Practice_6
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculate_Click(object sender, RoutedEventArgs e)
+        {
+            Pair firstPair = new Pair(int.Parse(FIRSTP1.Text), int.Parse(SECONDP1.Text));
+            Pair secondPair = new Pair(int.Parse(FIRSTP2.Text), int.Parse(SECONDP2.Text));
+            Pair resultPair = firstPair.Substract(secondPair);
+            result.Text = (resultPair.First, resultPair.Second).ToString();
+        }
+
+        private void ComparisonPair_Click(object sender, RoutedEventArgs e)
+        {
+            Pair pair = new Pair(int.Parse(FIRSTP1.Text), int.Parse(SECONDP1.Text));
+            if (pair)
+                result.Text = "true";
+            else result.Text = "false";
+        }
+        private void Information_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Золотарев М. А.\n Группа: ИСП-34\n Вариант №13", "Разработчик", MessageBoxButton.OK, MessageBoxImage.Information);
+        }
+        private void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            Close();
+        }
+    }
+}
